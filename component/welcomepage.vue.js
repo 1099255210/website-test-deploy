078f0ebf3704abf32af52f963aa911ce9b0546de
wudi_7mi <wongcarol2263@gmail.com>
2023-02-08 18:02:14 +0800

    exchange important dates and topics

--- component/welcomepage.vue.js
+++ component/welcomepage.vue.js
@@ -47,33 +47,6 @@ var welcomePage = {
 
   <section>
     <div class="container">
-      <p class="conf-title">Important Dates</p>
-      <div class="row">
-        <div class="col-md-6">
-
-          <div class="d-flex justify-content-between">
-            <p>Paper Submissions Deadline</p>
-            <p>31 July 2023</p>
-          </div>
-          <div class="d-flex justify-content-between">
-            <p>Notification</p>
-            <p>31 August 2023</p>
-          </div>
-          <div class="d-flex justify-content-between">
-            <p>Camera Ready Paper Deadline</p>
-            <p>21 September 2023</p>
-          </div>
-          <div class="d-flex justify-content-between">
-            <p>Conference Date</p>
-            <p>19-22 October 2023</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  </section>
-
-  <section>
-    <div class="container">
       <p class="conf-title">Topics</p>
       <div class="row">
         <div class="col-md-8">
@@ -113,6 +86,33 @@ var welcomePage = {
             <li>Operating systems for AIoT devices</li>
             <li>System-level energy management for AIoT devices</li>
           </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section>
+    <div class="container">
+      <p class="conf-title">Important Dates</p>
+      <div class="row">
+        <div class="col-md-6">
+
+          <div class="d-flex justify-content-between">
+            <p>Paper Submissions Deadline</p>
+            <p>31 July 2023</p>
+          </div>
+          <div class="d-flex justify-content-between">
+            <p>Notification</p>
+            <p>31 August 2023</p>
+          </div>
+          <div class="d-flex justify-content-between">
+            <p>Camera Ready Paper Deadline</p>
+            <p>21 September 2023</p>
+          </div>
+          <div class="d-flex justify-content-between">
+            <p>Conference Date</p>
+            <p>19-22 October 2023</p>
+          </div>
         </div>
       </div>
     </div>
