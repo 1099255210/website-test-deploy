95bb92ef1e09ea610b3763617775a7fcf32a7298
1099255210 <wudi7mi@gmail.com>
2023-08-23 11:26:50 +0800

    add testpreview

---
+++ component/testpreview.vue.js
@@ -0,0 +1,95 @@
+var testPreview = { 
+	template: `
+  <section class="p-5">
+    <div class="container">
+
+      <p class="main-title middle-set">
+        AIoTSys 2023 Registration Guide
+      </p>
+
+      <p class="registration-type"><strong> A. Paper registration </strong></p>
+      <p class="main-p middle-set">
+        <span class="bluestress">3,500 RMB</span>
+        to enjoy the right to participate (<span class="bluestress">3,000 RMB</span>
+        for Early Bird Price
+        )
+      </p>
+      <table class="registration-table" align="center">
+        <tr>
+          <td align="center">All discussions during the conference</td>
+          <td align="center">Conference materials in print</td>
+        </tr>
+        <tr>
+          <td align="center">Three lunches six coffee breaks</td>
+          <td align="center">The conference banquet</td>
+        </tr>
+      </table>
+      <p class="main-p">
+        Friendly Reminder:
+        <ol>
+          <li><span class="bluestress">All accepted papers require at least one standard registration.</span></li>
+          <li>Please provide your IEEE membership number during registration. For non-IEEE members, the standard registration fee is 5700 RMB.</li>
+          <li>Early bird registration fees: IEEE members - 4300 RMB, non-IEEE members - 5000 RMB. The early bird registration deadline is November 15th at 12:00 PM.</li>
+        </ol>
+      </p>
+
+      <p class="registration-type"><strong> B. Non-affiliate registration </strong></p>
+      <p class="main-p middle-set">
+        <span class="bluestress">2,300 RMB</span>
+        to enjoy the right to participate (<span class="bluestress">1,800 RMB</span>
+        for Early Bird Price
+        )
+      </p>
+      <table class="registration-table" align="center">
+        <tr>
+          <td align="center">All discussions during the conference</td>
+          <td align="center">Conference materials in print</td>
+        </tr>
+        <tr>
+          <td align="center">Three lunches six coffee breaks</td>
+          <td align="center">The conference banquet</td>
+        </tr>
+      </table>
+      <p class="main-p">
+        Friendly Reminder:
+        <ol>
+        <li><span class="bluestress">All accepted papers require at least one standard registration.</span></li>
+          <li>Please provide your IEEE membership number during registration. For non-IEEE members, the standard registration fee is 5700 RMB.</li>
+          <li>Early bird registration fees: IEEE members - 4300 RMB, non-IEEE members - 5000 RMB. The early bird registration deadline is November 15th at 12:00 PM.</li>
+        </ol>
+      </p>
+
+      <p class="registration-type"><strong> C. Alliance member registration </strong></p>
+      <p class="main-p middle-set">
+        <span class="bluestress">2,000 RMB</span>
+        to enjoy the right to participate (<span class="bluestress">1,500 RMB</span>
+        for Early Bird Price
+        )
+      </p>
+      <table class="registration-table" align="center">
+        <tr>
+          <td align="center">All discussions during the conference</td>
+          <td align="center">Conference materials in print</td>
+        </tr>
+        <tr>
+          <td align="center">Three lunches six coffee breaks</td>
+          <td align="center">The conference banquet</td>
+        </tr>
+      </table>
+      <p class="main-p">
+        Friendly Reminder:
+        <ol>
+        <li><span class="bluestress">All accepted papers require at least one standard registration.</span></li>
+          <li>Please provide your IEEE membership number during registration. For non-IEEE members, the standard registration fee is 5700 RMB.</li>
+          <li>Early bird registration fees: IEEE members - 4300 RMB, non-IEEE members - 5000 RMB. The early bird registration deadline is November 15th at 12:00 PM.</li>
+        </ol>
+      </p>
+      
+      <div class="middle-set padding-20">
+        <button onclick="redirect()" type="button" class="btn btn-primary btn-set redirect-reg">Start Registration</button>
+      </div>
+    </div>
+    
+  </section>
+  `
+};
