7faddcd85cf5d8e87cdb66b5043a89a78d35bdc1
1099255210 <wudi7mi@gmail.com>
2023-08-26 10:01:39 +0800

    20230826 upload registration

--- component/registration.vue.js
+++ component/registration.vue.js
@@ -2,9 +2,66 @@ var registration = {
 	template: `
   <section class="p-5">
     <div class="container">
-      <p class="conf-title">Registration</p>
-      <p> Comming Soon... </p>
+
+      <p class="main-title middle-set">
+        AIoTSys 2023 Registration Guide
+      </p>
+
+      <p class="registration-type"><strong> A. Paper Registration </strong></p>
+      <p class="main-p middle-set">
+        <span class="bluestress">3,500 RMB</span>
+        to enjoy the right to participate (<span class="bluestress">3,000 RMB</span>
+        for Early Bird Price
+        )
+      </p>
+      <table class="registration-table" align="center">
+        <tr>
+          <td align="center">All discussions during the conference</td>
+          <td align="center">Conference materials in print</td>
+        </tr>
+        <tr>
+          <td align="center">Paper Publication</td>
+          <td align="center">Lunches(21-22) and The conference banquet</td>
+        </tr>
+      </table>
+      <p class="main-p">
+      Friendly Reminder:
+      <ol>
+        <li><span class="bluestress">All accepted papers require at least one Paper Registration.</span></li>
+        <li>All participants must complete registration and join the conference group (on the registration page).</li>
+        <li>The early bird registration deadline is <span class="bluestress">September 19th</span>.</li>
+      </ol>
+    </p>
+
+      <p class="registration-type"><strong> B. Non-Paper Registration </strong></p>
+      <p class="main-p middle-set">
+        <span class="bluestress">2,300 RMB</span>
+        to enjoy the right to participate (<span class="bluestress">1,800 RMB</span>
+        for Early Bird Price
+        )
+      </p>
+      <table class="registration-table" align="center">
+        <tr>
+          <td align="center">All discussions during the conference</td>
+          <td align="center">Conference materials in print</td>
+        </tr>
+        <tr>
+          <td align="center">Lunches(21-22) and The conference banquet</td>
+        </tr>
+      </table>
+      <p class="main-p">
+        Friendly Reminder:
+        <ol>
+          <li>All participants must complete registration and join the conference group (on the registration page).</li>
+          <li>The early bird registration deadline is <span class="bluestress">September 19th</span>.</li>
+        </ol>
+      </p>
+      
+      <div class="middle-set padding-20">
+        <button onclick="redirect()" type="button" class="btn btn-primary btn-set redirect-reg">Start Registration</button>
+      </div>
     </div>
+    
   </section>
   `
 };
