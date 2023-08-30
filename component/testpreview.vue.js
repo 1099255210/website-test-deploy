54f3f771d471a831c96a52dc41c0e152aefe08fe
1099255210 <wudi7mi@gmail.com>
2023-08-30 09:50:27 +0800

    add AIoTSys Young Scientist Award

--- component/testpreview.vue.js
+++ component/testpreview.vue.js
@@ -2,66 +2,63 @@ var testPreview = {
 	template: `
   <section class="p-5">
     <div class="container">
+      <p>English/<a href="index.html#/testpreviewzh">中文</a></p>
+      <p class="conf-title">About AIoTSys Young Scientist Award</p>
 
-      <p class="main-title middle-set">
-        AIoTSys 2023 Registration Guide
-      </p>
+      <div class="row">
+        <div class="col-md-10">
+          <p class="main-p" style="text-align: justify">
+            Initiated by the AIoTSys International Conference, experts from ACM SIGBED CHINA, the Belt and Road International Intelligent Internet of Things Alliance (BRAVE), and the Intelligent Internet of Things Development and Education Innovation Alliance (IDEA) are jointly responsible for the selection, taking into account the purpose of professionalism, authority and talent incentive. This award aims to explore outstanding intelligent iot talents with strong development potential under the age of 35 (female aged 38) from all over the world, with research achievements, academic and technological influence, and community activity as evaluation indicators, 2 winners (1 overseas Chinese, 1 domestic Chinese) will be selected every year, and the results will be announced at the AIoTSys conference that year.
+          </p>
 
-      <p class="registration-type"><strong> A. Paper Registration </strong></p>
-      <p class="main-p middle-set">
-        <span class="bluestress">3,500 RMB</span>
-        to enjoy the right to participate (<span class="bluestress">3,000 RMB</span>
-        for Early Bird Price
-        )
-      </p>
-      <table class="registration-table" align="center">
-        <tr>
-          <td align="center">All discussions during the conference</td>
-          <td align="center">Conference materials in print</td>
-        </tr>
-        <tr>
-          <td align="center">Paper Publication</td>
-          <td align="center">Lunches(21-22) and The conference banquet</td>
-        </tr>
-      </table>
-      <p class="main-p">
-      Friendly Reminder:
-      <ol>
-        <li><span class="bluestress">All accepted papers require at least one Paper Registration.</span></li>
-        <li>All participants must complete registration and join the conference group (on the registration page).</li>
-        <li>The early bird registration deadline is <span class="bluestress">September 19th</span>.</li>
-      </ol>
-    </p>
-
-      <p class="registration-type"><strong> B. Non-Paper Registration </strong></p>
-      <p class="main-p middle-set">
-        <span class="bluestress">2,300 RMB</span>
-        to enjoy the right to participate (<span class="bluestress">1,800 RMB</span>
-        for Early Bird Price
-        )
-      </p>
-      <table class="registration-table" align="center">
-        <tr>
-          <td align="center">All discussions during the conference</td>
-          <td align="center">Conference materials in print</td>
-        </tr>
-        <tr>
-          <td align="center">Lunches(21-22) and The conference banquet</td>
-        </tr>
-      </table>
-      <p class="main-p">
-        Friendly Reminder:
-        <ol>
-          <li>All participants must complete registration and join the conference group (on the registration page).</li>
-          <li>The early bird registration deadline is <span class="bluestress">September 19th</span>.</li>
-        </ol>
-      </p>
-      
-      <div class="middle-set padding-20">
-        <button onclick="redirect()" type="button" class="btn btn-primary btn-set redirect-reg">Start Registration</button>
-      </div>
-    </div>
+          <p class="conf-subtitle">Selection Criteria</p>
+          
     
+          <p class="main-p">
+          Candidates for the AIoTSys Young Scientist Award must be Chinese AIoT technical talent aged 35 years (female 38 years), and the selection criteria include but are not limited to: 
+            <ul>
+              <li>Major journal articles; (no more than 5 articlesrs) </li>
+              <li>Project chairmanship and participation</li>
+              <li>Get honors, such as talent programs </li>
+              <li>Social work situation </li>
+              <li>Two recommended experts</li>
+            </ul>
+          </p>
+
+          <p class="conf-subtitle">Selection process</p>
+
+          <p class="main-p">
+            <ul>
+              <li>Application and recommendation: September 1 - September 30, 2023 </li>
+              <li>First Review: October 1 - October 5, 2023 </li>
+              <li>Final adjudication: from October 6 to October 15, 2023, Final results will be posted on AIoTSys (registration and attendance required)</li>
+            </ul>
+          </p>
+
+          <p class="conf-subtitle">Selection Committee</p>
+
+          <p class="main-p">
+            <p>Tsao Kinong, Hong Kong Polytechnic University </p>
+            <p>Liu Jie, Harbin Institute of Technology (Shenzhen) </p>
+            <p>Zhang Yanyong, University of Science and Technology of China </p>
+            <p>Guo Bin, Northwestern Polytechnical University</p>
+
+            <p><strong>Contact person of the Selection Committee: </strong></p>
+            <a href="mailto:guob@nwpu.edu.cn">Guo Bin, guob@nwpu.edu.cn</a> 
+          </p>
+
+          <p class="conf-subtitle">Application of AIoTSys Young Scientist Award</p>
+
+          <p class="main-p">
+          Applicants should complete the AIoTSys Young Scientist Application Form 
+          <strong><a href="./attachments/AIoTSys Young Scientist Award.docx">(AIoTSys Young Scientist Application Form) </a></strong>
+          and send the application form and attachments to <a href="mailto:guob@nwpu.edu.cn">guob@nwpu.edu.cn</a> .
+          </p>
+
+        </div>
+      </div>
+
+    </div>
   </section>
   `
 };
---
+++ component/testpreviewzh.vue.js
@@ -0,0 +1,64 @@
+var testPreviewZh = { 
+	template: `
+  <section class="p-5">
+    <div class="container">
+      <p><a href="index.html#/testpreview">English</a>/中文</p>
+      <p class="conf-title">关于AIoTSys青年科学家奖</p>
+
+      <div class="row">
+        <div class="col-md-10">
+          <p class="main-p" style="text-align: justify">
+          由AIoTSys国际会议发起，ACM SIGBED CHINA、一带一路国际智能物联网联盟（BRAVE）、智能物联网发展及其教育创新联盟（IDEA）三团体的专家联合负责评选，兼顾专业性、权威性和人才激励。旨在从全球挖掘 35 岁（女性38岁）以下具有极强发展潜力的优秀智能物联网人才，以研究成果、学术与科技影响力、社区活跃度等作为评价指标，每年评选2位（海外华人1位，国内华人1位），并于当年AIoTSys会议上公布评选结果。
+          </p>
+
+          <p class="conf-subtitle">评选标准</p>
+          
+    
+          <p class="main-p">
+          AIoTSys青年科学家奖参选人须为 35 岁（女性38岁）华人AIoT技术人才，评选标准包括但不限于：
+            <ul>
+              <li>代表性论文（不超过5篇）</li>
+              <li>主持以及参与项目情况 </li>
+              <li>获得荣誉，如人才计划等 </li>
+              <li>社会工作情况 </li>
+              <li>两位推荐人</li>
+            </ul>
+          </p>
+
+          <p class="conf-subtitle">评选流程</p>
+
+          <p class="main-p">
+            <ul>
+              <li>报名及推荐：2023 年 9 月 1 日至 9 月 30 日</li>
+              <li>初审：2023 年 10 月 1 日至 10 月 5 日</li>
+              <li>终审：2023 年 10 月 6 日至 10 月 15 日，最终结果在AIoTSys上公布（需本人注册并参加会议）</li>
+            </ul>
+          </p>
+
+          <p class="conf-subtitle">评选委员会</p>
+
+          <p class="main-p">
+            <p>曹建农，香港理工大学</p>
+            <p>刘劼，哈尔滨工业大学（深圳） </p>
+            <p>张燕咏，中国科学技术大学</p>
+            <p>郭斌，西北工业大学</p>
+
+            <p><strong>评选委员会联系人： </strong></p>
+            <a href="mailto:guob@nwpu.edu.cn">Guo Bin, guob@nwpu.edu.cn</a> 
+          </p>
+
+          <p class="conf-subtitle">申请方式</p>
+
+          <p class="main-p">
+          申请人请填写AIoTSys青年科学家申请表
+          <strong><a href="./attachments/AIoTSys Young Scientist Award.docx">(AIoTSys Young Scientist Application Form) </a></strong>
+          ，并发送申请表与附件至 <a href="mailto:guob@nwpu.edu.cn">guob@nwpu.edu.cn</a> .
+          </p>
+
+        </div>
+      </div>
+
+    </div>
+  </section>
+  `
+};
