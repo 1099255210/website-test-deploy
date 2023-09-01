748f49f5af9192d65e9bf2443a61b83128823f73
1099255210 <wudi7mi@gmail.com>
2023-09-01 17:10:50 +0800

    20230901 updated cr

--- component/papersubmission/camerareadyinstructions.vue.js
+++ component/papersubmission/camerareadyinstructions.vue.js
@@ -3,7 +3,90 @@ var cameraReadyInstructions = {
   <section class="p-5">
     <div class="container">
       <p class="conf-title">Camera Ready Instructions</p>
-      <p> Comming Soon... </p>
+
+      <div class="row">
+        <div class="col-md-10">
+          <p class="main-p" style="text-align: justify">
+          Authors of accepted papers should prepare a camera-ready (final) version of their paper and submit it using the EasyChair system no later than <strong>Sunday, September 10, 2023</strong>. Failure to submit by the deadline will result in an automatic withdrawal of the paper from the conference and the proceedings.  
+          </p>
+
+          <p class="main-p">ANNOUNCEMENT: The "AIoTSys 2023 Best Paper Award" will be selected by the Program Chairs. All invited papers are eligible for this award. </p>
+          
+    
+          <p class="main-p">
+          To submit your camera-ready (final) paper, please follow the four steps below: 
+            <ul>
+              <li>Submit the IEEE Copyright Form through EasyChair.</li>
+              <li>Prepare your Camera-Ready (Final) Paper using a template provided by IEEE. </li>
+              <li>Verify that your Camera-Ready (final) paper is IEEE Xplore compliant.</li>
+              <li>Submit your Camera-Ready (final) PDF file electronically thru EasyChair.</li>
+            </ul>
+          </p>
+
+          <p class="conf-subtitle">​Step 1: Submit the IEEE Copyright Form through EasyChair</p>
+
+          <p class="main-p">IEEE requires that an author of every published paper sign a copyright agreement. As we add each accepted paper to the proceedings, the system will automatically send an email to each of the corresponding authors with detailed instructions on how to sign the copyright agreement through EasyChair. This form should be signed and submitted by the primary author before the camera-ready PDF is submitted. </p>
+          
+          <p class="main-p">Please review and update all author information in EasyChair prior to submitting the Copyright form. The final author information for the proceedings will pull from EasyChair and not the final camera-ready paper itself. The IEEE Copyright Form must also match the EasyChair author demographic information exactly.</p>
+          
+          <p class="main-p">NOTE: By signing the IEEE copyright form, you are affirming that the material in your paper is original and you have not previously released copyright for this paper to another party.  Failure to submit the IEEE copyright form by the deadline will result in an automatic withdrawal of your paper for presentation and publication.</p>
+          
+          <p class="main-p">Review the detailed IEEE Copyright Policy here.</p>
+          
+          <p class="conf-subtitle">Step 2: Prepare your Camera-Ready (Final) Paper</p>
+          
+          <p class="main-p">
+          Please use one of the approved templates provided by IEEE to prepare your camera-ready (final) paper for submission:  https://www.ieee.org/conferences/publishing/templates.html.
+          </p>
+          
+          <p class="main-p">Camera-ready (final) papers should mandatorily address the reviewers' comments.  The AIoTSys 2023 Program Chairs reserve the right to reject a camera-ready (final) paper if the reviewers' comments are not adequately addressed. The camera-ready paper should also contain all the necessary details regarding the authors and their affiliations.</p>
+          
+          <p class="conf-subtitle">Step 3: Verify that your camera-ready (final) paper is IEEE Xplore compliant</p>
+
+          <p class="main-p">All papers published in the AIoTSys 2023 Proceedings and presented at the conference will be submitted for inclusion into IEEE Xplore®. Therefore, all camera-ready (final) paper files must adhere to the IEEE Xplore PDF specifications for compatibility.</p>
+
+          <p class="main-p">In order to facilitate the process of creating and verifying PDF files, we have registered for use of the tool IEEE PDF eXpress. This is a free service for IEEE conferences, allowing authors to convert their final Word or LaTeX file into an IEEE Xplore-compliant PDF - or to check PDFs that authors made themselves for IEEE Xplore-compliance. </p>
+          
+          <p class="main-p">
+          In order to verify that your camera-ready (final) paper is IEEE Xplore-compliant please follow the steps below:
+          
+          </p>
+          
+          <p class="main-p">Go to the IEEE PDF eXpress (https://ieee-pdf-express.org/account/Login) web site.</p>
+          
+          <p class="main-p">If you already have an IEEE eXpress account (either for AIoTSys 2023 or another conference) - log in using our Conference ID 58602X, your email address, and password.   </p>
+          
+          <p class="main-p">If you are a first-time user, create an account on the IEEE PDF eXpress system. To do that, click on the “New Users – Click Here” link. Enter the Conference ID 58602X, your email address, and choose a new password. Continue to enter information as prompted. You will then receive online and email confirmation of a successful account setup. You can now proceed to login to the IEEE eXpress system. </p>
+          
+          <p class="main-p">For each AIoTSys 2023 camera-ready (final) paper that you want to process, click "Create New Title", enter identifying text for the paper (the paper title is recommended), and then click "Submit PDF for Checking" or "Submit Source File for Conversion".  You will now be prompted to upload your file. Click "Browse" to browse to your file and then click “Upload File”. </p>
+          
+          <p class="main-p">Once the checking/conversion process is completed successfully, the account holder will receive an email with the verified or converted PDF file attached to it.  This approved PDF file is labeled within its document properties as being "Certified by IEEE PDF eXpress", with an exact date and time stamp.  This approved file - and not your original upload - is what you must submit to EasyChair as your camera-ready (final) paper. </p>
+          
+          <p class="main-p">VERY IMPORTANT: Name your camera-ready (final) file using "CR-presenting author last name-EasyChair paper ID”  (ex "CR-Morris-68").</p>
+          
+          <p class="main-p">For more detailed instructions and assistance with error messages, please refer to the “Using IEEE PDF eXpress” website here:  https://www.pdf-express.org/frhelpnologin.asp.</p>
+
+          <p class="conf-subtitle">Step 4: Submit your camera-ready (final) PDF file electronically through EasyChair.</p>
+
+          <p class="main-p">Be sure to submit the approved camera-ready (final) PDF file that you received via email from IEEE PDF eXpress. This approved PDF file is labeled within its document properties as being "Certified by IEEE PDF eXpress", with an exact date and time stamp.</p>
+          
+          <p class="main-p">​To submit your camera-ready (final) PDF file electronically please follow the steps below:</p>
+          
+          <p class="main-p">Login to EasyChair as you did to submit the original paper.</p>
+          
+          <p class="main-p">Ensure ALL paper authors are included on the author list, and in the correct order.</p>
+          
+          <p class="main-p">Check that all paper information in EasyChair matches the information on the camera-ready PDF file. Detailed information (title, abstract, etc) that will appear in the conference proceedings will be taken from EasyChair and not from the submitted PDF file.</p>
+          
+          <p class="main-p">Click on the “Update File” link, browse to the approved PDF, then upload.  Depending on the size of your file and your internet connection speed, the file upload may take a few minutes. If the file is uploaded successfully then a confirmation message will be displayed. You will also receive an e-mail confirmation with the details of your submission.</p>
+          
+          <p class="main-p">As required by IEEE, all accepted papers will be scanned for possible plagiarism using CrossCheck, prior to publication in the proceedings.</p>
+          
+          <p class="main-p">All conference attendees are required to review and adhere to the IEEE non-discrimination policy on the Registration page.</p>
+
+        </div>
+      </div>
+
     </div>
   </section>
   `
