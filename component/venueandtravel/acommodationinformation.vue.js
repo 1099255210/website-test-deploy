e6d29f5e60fb396bc500ebf520da2f05e8a668e7
wudi_7mi <122330888+wudi-7mi@users.noreply.github.com>
2023-09-26 14:43:44 +0800

    20230926 hotel update

--- component/venueandtravel/acommodationinformation.vue.js
+++ component/venueandtravel/acommodationinformation.vue.js
@@ -3,7 +3,36 @@ var acommodationInformation = {
   <section class="p-5">
     <div class="container">
       <p class="conf-title">Acommodation Information</p>
-      <p> Comming Soon... </p>
+
+      <div class="row">
+
+        <div class="col-8">
+          <p class="main-p bluestress">订房请报会议名称：2023 AIoTSys 智能物联网系统国际会议</p>
+        
+          <p class="conf-subtitle">推荐酒店A(会议主会场) - 唐隆国际酒店</p>
+
+          <p class="main-p">订房客户经理 贺萌 13772516615</p>
+          <p class="main-p">协议价：610元（含早）</p>
+          <p class="main-p">高级双人间 / 高级单人间</p>
+          <p class="main-p">地址：陕西省西安市雁塔区雁南四路98号</p>
+          
+          <p class="conf-subtitle">推荐酒店B - 西安曲江智选假日中心酒店</p>
+          
+          <p class="main-p">距离会场1.2公里，步行15分钟，驾车5分钟</p>
+          <p class="main-p">订房客户经理 张帆 13572171345 </p>
+          <p class="main-p">协议价：428元 单双同价（含早）</p>
+          <p class="main-p">高级双床房 / 高级大床</p>
+          <p class="main-p">陕西省西安市雁塔区文丰路西安文化科技创意城G座</p>
+          
+          <p class="conf-subtitle">其它酒店</p>
+        </div>
+        <img class="img-fluid" src="./assets/hotel.png">
+        
+      </div>
+
+      
+
+
     </div>
   </section>
   `
