b8d1590f8f826d56b970b9642b3344723d02c5f5
wudi_7mi <122330888+wudi-7mi@users.noreply.github.com>
2023-09-26 09:33:10 +0800

    20230926 updae

--- component/program/program.vue.js
+++ component/program/program.vue.js
@@ -4,7 +4,13 @@ var program = {
     <div class="container">
       <p class="conf-title">Program</p>
       <div class="row">
-        <img src="./assets/push/schedule.png">
+        <img src="./assets/push/arr_01.jpg">
+        <img src="./assets/push/arr_02.jpg">
+        <img src="./assets/push/arr_03.jpg">
+        <img src="./assets/push/arr_04.jpg">
+        <img src="./assets/push/arr_05.jpg">
+        <img src="./assets/push/arr_06.jpg">
+        <img src="./assets/push/arr_07.jpg">
       </div>
     </div>
   </section>
